6ef5d13f8c0c34017b6c8feae7a196b85fd63b26
ForceBru <mia.m5@yandex.ru>
2019-04-09 00:31:26 +0300

    Started ELF parsing testing.

---
+++ VM/ELF/ELF_parser.py
@@ -0,0 +1,108 @@
+import struct
+
+__all__ = ('ELF_parser', )
+
+class ELF_meta(type):
+    def __new__(cls, name, bases, dict):
+        if '__annotations__' not in dict:
+            return super().__new__(cls, name, bases, dict)
+            
+        layouts = {}
+        current_entries = []
+        current_layout = ''
+        for entry, annot in dict['__annotations__'].items():
+            if isinstance(annot, cls):
+                if current_layout:
+                    layouts[tuple(current_entries)] = current_layout
+                    current_entries, current_layout = [], ''
+                layouts[('_' + entry, )] = annot
+            elif isinstance(annot, str):
+                current_entries.append('_' + entry)
+                current_layout += annot
+            else:
+                raise ValueError(f'Unexpected type in annotation for {name}.{entry}: {annot!r} of type {type(annot)}')
+            
+            try:
+                converter = dict[entry]  # must be a function of one argument
+                if not hasattr(converter, '__call__'):
+                    raise ValueError(f'Converter for entry {name}.{entry} must be callable (got {converter!r})')
+            except KeyError:
+                converter = lambda x: x
+                
+            dict[entry] = property(lambda self, e=entry, c=converter: c(self.__getattribute__('_' + e)))
+            dict['_' + entry] = 0
+        
+        if current_layout:
+            layouts[tuple(current_entries)] = current_layout
+        
+        dict['_layouts'] = {
+            key: (
+                struct.Struct(value)
+                if isinstance(value, str) else value
+                )
+            for key, value in layouts.items()
+            }
+        dict['_name'] = name
+        
+        def __init__(self, stream):
+            self.__RAW = b''
+            for entries, parser in self._layouts.items():
+                if isinstance(parser, struct.Struct):
+                    _raw = stream.read(parser.size)
+                    parsed = parser.unpack(_raw)
+                    self.__RAW += _raw
+                else:
+                    parsed = parser(stream),
+                    self.__RAW += parsed[0].__RAW
+                    
+                for name, value in zip(entries, parsed):
+                    assert name.startswith('_')
+                    setattr(self, name, value)
+                
+            self.__repr = None
+            self.__len = len(self.__RAW)
+                
+        def __repr__(self):
+            if self.__repr is None:
+                ret = self._name + ' {\n'
+                for entries, parser in self._layouts.items():
+                    if isinstance(parser, struct.Struct):
+                        for name in entries:
+                            data = self.__getattribute__(name[1:])
+                            if isinstance(data, bytes):
+                                data = '0x' + data.hex()
+                            ret += f'\t{name[1:]} = {data},\n'
+                    else:
+                        for name in entries:
+                            data = self.__getattribute__(name)
+                            lines = repr(data).splitlines()
+                            data = lines[0] + '\n' + '\n'.join('\t' + s for s in lines[1:])
+                            ret += f'\t{name[1:]} = {data},\n'
+                self.__repr = ret + '}'
+            return self.__repr
+            
+        def __bytes__(self):
+            return self.__RAW
+            
+        def __eq__(self, other):
+            return self.__RAW == other.__RAW
+            
+        def __hash__(self):
+            return hash(self.__RAW)
+            
+        def __len__(self):
+            return self.__len
+                
+        dict['__init__'] = __init__
+        dict['__repr__'] = __repr__
+        dict['__bytes__'] = __bytes__
+        dict['__eq__'] = __eq__
+        dict['__hash__'] = __hash__
+        dict['__len__'] = __len__
+            
+        return super().__new__(cls, name, bases, dict)
+
+
+class ELF_parser(metaclass=ELF_meta):
+    ...
+
---
+++ VM/ELF/analyze.py
@@ -0,0 +1,66 @@
+from ELF import ELF32_Ehdr, ELF32_Shdr
+from enums import EI_CLASS, EI_DATA, e_machine
+
+
+class ELF32:
+    def __init__(self, fname: str):
+        self.fname = fname
+        self.file = open(self.fname, 'rb')
+        self.hdr = ELF32_Ehdr(self.file)
+        
+        ident = self.hdr.e_ident
+        
+        assert ident.EI_MAG0 == 127\
+        and ident.EI_MAG1 == 69\
+        and ident.EI_MAG2 == 76\
+        and ident.EI_MAG3 == 70, f'Invalid header in file {self.fname!r}'
+        
+        assert ident.EI_CLASS == EI_CLASS.ELFCLASS32, f'64-bit architecture is not supported (file {self.fname!r})'
+        
+        assert ident.EI_DATA == EI_DATA.ELFDATA2LSB, f'Big endian is not supported (file {self.fname!r})'
+        
+        assert self.hdr.e_machine == e_machine.x86, f'Architecture {self.hdr.e_machine} is not supported (file {self.fname!r})'
+        
+        self.__sections = None
+        self.__sections_contents = {}
+        
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, *args):
+        self.file.close()
+        
+    @property
+    def sections(self):
+        if self.__sections is None:
+            self.file.seek(self.hdr.e_shoff)
+            sections = [ELF32_Shdr(self.file) for _ in range(self.hdr.e_shnum)]
+    
+            shstrndx = sections[self.hdr.e_shstrndx]
+            self.file.seek(shstrndx.sh_offset)
+            names = self.file.read(shstrndx.sh_size)
+        
+            self.__sections = {
+                names[section.sh_name:].split(b'\0', 1)[0].decode():
+                    section
+                for section in sections[1:]
+            }
+            
+        return self.__sections
+
+    def get_section_contents(self, section_name: str) -> bytes:
+        try:
+            return self.__sections_contents[section_name]
+        except KeyError:
+            section = self.sections[section_name]
+            self.file.seek(section.sh_offset)
+            
+            self.__sections_contents[section_name] = self.file.read(section.sh_size)
+            
+        return self.__sections_contents[section_name]
+
+
+if __name__ == '__main__':
+    with ELF32('samples/hello_world') as ELF:
+        print(ELF.hdr)
+        print(ELF.sections)
