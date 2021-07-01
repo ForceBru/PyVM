dceac30d68f28b6a31fa8eae6e7154200df48e7a
Mr. Hallway <git@mrhallway.me>
2021-07-01 11:37:45 -0700

    Added comments ¯\_(ツ)_/¯.

--- VM/__init__.py
+++ VM/__init__.py
@@ -1,16 +1,18 @@
+# Import modules
 import sys
-
 from .CPU import CPU32
 from .kernel import Kernel
 from .fetchLoop import FetchLoopMixin, ExecuteBytes, ExecuteFlat, ExecuteELF, ExecutionStrategy
 
+# Create author and version vars
 __author__ = '@ForceBru'
 __version__ = 0, 1, 0
 
 
+# Create VM class
 class VM(CPU32, FetchLoopMixin):
-    __slots__ = 'fmt', 'descriptors', 'GDT', 'running', 'RETCODE', 'kernel'
-    from .misc import process_ModRM
+    __slots__ = 'fmt', 'descriptors', 'GDT', 'running', 'RETCODE', 'kernel' # Specify slots
+    from .misc import process_ModRM # Import module
 
     def __init__(self, memsize: int, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
         super().__init__(int(memsize))
@@ -19,7 +21,7 @@ class VM(CPU32, FetchLoopMixin):
         self.descriptors = [stdin, stdout, stderr]
         self.GDT = [
             b'\0' * 8  # 64-bit entry
-        ] * 6  # TODO: how many entries are there?
+        ] * 6          # TODO: how many entries are there?  ¯\_(ツ)_/¯
         self.RETCODE = None
 
     def interrupt(self, code: int):
@@ -28,10 +30,10 @@ class VM(CPU32, FetchLoopMixin):
 
             self.reg.eax = self.kernel[syscall_number]()
         else:
-            raise RuntimeError(f'Interrupt 0x{code:02x} is not supported yet')
+            raise RuntimeError(f'Interrupt 0x{code:02x} is not supported yet') # Call a RuntimeError if code is not 0x80. 
 
-
-class VMKernel(VM, ExecuteELF, ExecuteBytes, ExecuteFlat):
+# Create VMKernel class
+class VMKernel(VM, ExecuteELF, ExecuteBytes, ExecuteFlat): 
     def execute(self, strategy: ExecutionStrategy, *args, **kwargs):
         bases = {
             ExecutionStrategy.BYTES: ExecuteBytes,
